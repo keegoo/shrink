d8df137c8d8cc8db744424a13a13eed272cd3094
CY <onions.yang@gmail.com>
2017-07-31 21:21:17 +0800

    initialize URL controller and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170731131426) do
+
+  create_table "urls", force: :cascade do |t|
+    t.string   "shorten"
+    t.string   "origin"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end

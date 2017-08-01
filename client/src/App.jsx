309ab6d442fe98cec52736c041d641fd2ed9dc65
CY <onions.yang@gmail.com>
2017-08-01 16:42:39 +0800

    add Editor component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-import Menu from './components/Menu.jsx'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
+import Menu from './components/Menu.jsx'
+import Editor from './components/Editor.jsx'
+
+const styles = {
+  editor: {
+    width: '60%',
+    margin: 'auto'
+  }
+}
 class App extends React.Component {
   render(){
     return(
       <div>
         <Menu title="Shrink your URL"/>
-        <h1>React APP!</h1>
+        <div style={styles.editor}>
+          <Editor />
+          <br/>
+        </div>
       </div>
     )
   }
---
+++ client/src/components/Editor.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import TextField from 'material-ui/TextField'
+import { Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn } from 'material-ui/Table'
+
+class Editor extends React.Component {
+  state = {
+    data: [
+      { id: 1, shorten: 'el', origin: 'http://www.baidu.com', visit: 4 },
+      { id: 2, shorten: 'em', origin: 'http://www.sina.com.cn', visit: 4 },
+      { id: 3, shorten: 'en', origin: 'http://www.google.com', visit: 4 },
+      { id: 4, shorten: 'eo', origin: 'http://www.github.com', visit: 4 },
+      { id: 5, shorten: 'ep', origin: 'http://www.weibo.com', visit: 4 },
+      { id: 6, shorten: 'eq', origin: 'http://www.wechat.com', visit: 4 },
+      { id: 7, shorten: 'er', origin: 'http://www.tencent.com', visit: 4 }
+    ]
+  }
+
+  render() {
+    return(
+      <div>
+        <TextField
+          hintText="e.g.: http:// ..."
+          floatingLabelText="input your URL here"
+        />
+        <br/>
+        <br/>
+        <Table multiSelectable={false}>
+          <TableHeader adjustForCheckbox={false} displaySelectAll={false}>
+            <TableRow>
+              <TableHeaderColumn colSpan="4" >
+                <h2 style={{textAlign: 'center'}}>Dashboard</h2>
+              </TableHeaderColumn>
+            </TableRow>
+          </TableHeader>
+          <TableBody displayRowCheckbox={false}>
+            {
+              this.state.data.map((v, index) => {
+                return(
+                  <TableRow key={v.id}>
+                    <TableRowColumn>{v.id}</TableRowColumn>
+                    <TableRowColumn>{v.shorten}</TableRowColumn>
+                    <TableRowColumn>{v.origin}</TableRowColumn>
+                    <TableRowColumn>{v.visit}</TableRowColumn>
+                  </TableRow>
+                )
+              })
+            }
+          </TableBody>
+        </Table>
+      </div>
+
+    )
+  }
+}
+
+export default Editor
